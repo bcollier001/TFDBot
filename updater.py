fb3572ef7ec33a4398b7d17dfc1df247971b05c0
Brandon Collier <58408434+bcollier001@users.noreply.github.com>
2024-10-03 16:46:27 -0500

    Update updater.py

--- updater.py
+++ updater.py
@@ -12,80 +12,55 @@ headers = {"x-nxopen-api-key": f"{api_key}"}
 
 Items = ['descendant','weapon','module','reactor','external-component','reward','stat','void-battle']
 
-def remove_jsonData(sfolder = None):
-    if sfolder is not None:
-        if sfolder in Items:
-            folders_removed = 0
-            files_removed = 0
-            #purge jsonData in specified folder
-            for data in os.listdir(f'./jsonData/{sfolder}s'):
-                os.remove(f'./jsonData/{sfolder}s/{data}')
-                files_removed += 1
-            os.rmdir(f'./jsonData/{sfolder}s')
-            folders_removed += 1
+def remove_json(file = None):
+    files_removed = []
+    items_removed = 0
+    if file is not None:
+        file = file.lower()
+        if file in Items:
+            with open(f"jsonData/{file}s.json") as f:
+                items_removed += len(json.load(f))
+            os.remove(f"jsonData/{file}s.json")
+            files_removed.append(file + 's')
         else:
-            return False, 0, 0, f"Invalid folder name: {sfolder}s"
+            return False, [], 0, 0, f"Invalid file name: {file}s"
     else:
-        #purge all jsonData
-        folders_removed = 0
-        files_removed = 0
-        for folder in os.listdir('./jsonData'):
-            for file in os.listdir(f'./jsonData/{folder}'):
-                os.remove(f'./jsonData/{folder}/{file}')
-                files_removed+=1
-            os.rmdir(f'./jsonData/{folder}')
-            folders_removed+=1
-    return True, folders_removed, files_removed, None
-
-def get_jsonData(sfolder = None):
-    if sfolder is not None:
-        if sfolder in Items:
-            newpath = f'./jsonData/{sfolder}s'
-            if not os.path.exists(newpath):
-                os.makedirs(newpath)
-            urlString = f"{api_url}/static/tfd/meta/en/{sfolder}.json"
+        for file in os.listdir('jsonData'):
+            with open(f"jsonData/{file}") as f:
+                items_removed += len(json.load(f))
+            os.remove(f"jsonData/{file}")
+            files_removed.append(file[:-5])
+    return True, files_removed, items_removed, None
+        
+        
+def fetch_json(file = None):
+    files_fetched = []
+    items_fetched = 0
+    if file is not None:
+        file = file.lower()
+        if file in Items:
+            urlString = f"{api_url}/static/tfd/meta/en/{file}.json"
             response = requests.get(urlString, headers = headers)
             if response.status_code == 200:
-                parsed_files = parse_data(response.json(), sfolder)
-                return True, 1, parsed_files, None
+                json_data = response.json()
+                items_fetched += len(json_data)
+                with open(f"jsonData/{file}s.json", 'w') as f:
+                    json.dump(json_data, f, indent=4)
+                    files_fetched.append(file + 's')
             else:
-                return False, 0, 0, f"Error: response code is {response.status_code}"
-
+                return False, files_fetched, items_fetched, f"Response Code: {response.status_code}"
         else:
-            return False, 0, 0, f"Invalid folder name: {sfolder}s"
+            return False, files_fetched, items_fetched, f"Invalid file name: {file}s"
     else:
-        #get all jsonData
-        total_parsed_files = 0
-        folders_processed = 0
         for item in Items:
-            newpath = f'./jsonData/{item}s'
-            if not os.path.exists(newpath):
-                os.makedirs(newpath)
             urlString = f"{api_url}/static/tfd/meta/en/{item}.json"
             response = requests.get(urlString, headers = headers)
-            parsed_files = parse_data(response.json(), item)
-            total_parsed_files += parsed_files
-            folders_processed += 1
-        #-1 total parsed files because idk why it's not working
-        return True, folders_processed, total_parsed_files-1, None
-
-def parse_data(dataFile, item: str):
-    parsed_data = 0
-    for data in dataFile:
-        if item == 'reward':
-            map_id = data.get('map_id').replace(' ','_')
-            if map_id.startswith('610001') or map_id.startswith('610002'):
-                item_name = f"{data.get('map_name').replace(' ','_')}"
+            if response.status_code == 200:
+                json_data = response.json()
+                items_fetched += len(json_data)
+                with open(f"jsonData/{item}s.json", 'w') as f:
+                    json.dump(json_data, f, indent=4)
+                    files_fetched.append(item + 's')
             else:
-                item_name = f"{map_id} - {data.get('map_name').replace(' ','_')}"
-            filename = f"jsonData/{item}s/{item_name}.json"
-
-        else:
-            item_id = data.get(f"{item.replace('-','_')}_id").replace(' ','_').replace('\'','')
-            item_name = data.get(f"{item.replace('-','_')}_name").replace(' ','_').replace('\'','')
-            filename = f"jsonData/{item}s/{item_name} - {item_id}.json"
-
-        with open(filename, 'w') as f:
-            json.dump(data, f, indent=4)
-        parsed_data += 1
-    return parsed_data
+                return False, files_fetched, items_fetched, f"Response Code: {response.status_code}"
+    return True, files_fetched, items_fetched, None
